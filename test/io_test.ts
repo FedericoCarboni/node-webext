39e03ce2d184d6a0b2b5e2980a970c6ae1e95263
Federico Carboni <fr3ddydev@gmail.com>
2020-12-27 18:31:16 +0100

    test: add tests for io

--- test/io_test.ts
+++ test/io_test.ts
@@ -1,20 +1,50 @@
-// import crypto from 'crypto';
-import fs from 'fs';
-import { recv, send } from '../src/io';
+import { PassThrough } from 'stream';
+import { send, recv } from '../src/io';
 
-describe('send()', () => {
-  it('should send a message', async () => {
-    const stdout = fs.createWriteStream('stdout');
-    const str = Buffer.alloc(10000, 0);
-    await send({ A: str.toString('base64'), B: str.toString('base64') }, { stdout });
-    stdout.close();
+describe('io', () => {
+  describe('send()', () => {
+    it('should throw TypeError if stdout is not writable', async () => {
+      const stream = new PassThrough();
+      stream.end();
+      await expect(send({}, { stdout: stream })).rejects.toBeInstanceOf(TypeError);
+    });
+    it('should throw RangeError if >1MB', async () => {
+      const stream = new PassThrough();
+      await expect(send({
+        message: 'A'.repeat(1000000),
+      }, { stdout: stream })).rejects.toBeInstanceOf(RangeError);
+      stream.end();
+    });
   });
-});
-describe('recv()', () => {
-  it('should recv a message', async () => {
-    const stdin = fs.createReadStream('stdout');
-    await recv({ stdin });
-    // console.log(message.length);
-    stdin.close();
+  describe('recv()', () => {
+    it('should throw TypeError if stdin is not readable', async () => {
+      const stream = new PassThrough();
+      stream.end();
+      stream.destroy();
+      stream.read();
+      await new Promise((resolve) => stream.on('close', resolve));
+      await expect(recv({ stdin: stream })).rejects.toBeInstanceOf(TypeError);
+    });
+    it('should throw RangeError if >1MB', async () => {
+      const stream = new PassThrough();
+      await send({
+        message: 'A'.repeat(1000),
+      }, {
+        stdout: stream,
+      });
+      await expect(recv({
+        maxSize: 1000,
+        stdin: stream,
+      })).rejects.toBeInstanceOf(RangeError);
+      stream.end();
+    });
+  });
+  it('should send and receive messages', async () => {
+    const message = 'if you can read me it worked';
+    const stream = new PassThrough();
+    await send({ message }, { stdout: stream });
+    const received = await recv<any>({ stdin: stream });
+    expect(received.message).toBe(message);
+    stream.end();
   });
 });
