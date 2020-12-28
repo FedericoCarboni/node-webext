6df582a316632bb5e5c11b61001a953e6e722d15
Federico Carboni <fr3ddydev@gmail.com>
2020-12-28 10:12:18 +0100

    build: fix test and build config

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,26 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 
 const gulp = require('gulp');
 const header = require('gulp-header');
 const ts = require('gulp-typescript');
 
+const rollup = require('rollup');
+/** @type {import('@rollup/plugin-typescript').default} */ // @ts-ignore
+const typescript = require('@rollup/plugin-typescript');
+
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
+const { builtinModules } = require('module');
 
 const tsTestProject = ts.createProject('test/integration/tsconfig.json');
-const tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json', {
+  rootDir: './',
+  module: 'commonjs',
+});
 
 gulp.task('launchChrome', async () => {
   const chromeOptions = new chrome.Options();
@@ -50,3 +59,28 @@ gulp.task('buildTests', gulp.series(function moveMochaAndChai() {
 }));
 
 gulp.task('test:chrome', gulp.series('buildTests', 'launchChrome'));
+
+gulp.task('build', async () => {
+  await fs.promises.writeFile('dist/es/package.json', JSON.stringify({ type: 'module' }, null, 2));
+  const bundle = await rollup.rollup({
+    input: 'src/index.ts',
+    plugins: [
+      typescript(),
+    ],
+    external: [...builtinModules],
+  });
+  await bundle.write({
+    file: 'dist/es/index.js',
+    format: 'es',
+    preferConst: true,
+  });
+  await bundle.write({
+    dir: 'dist/',
+    format: 'cjs',
+    preferConst: true,
+  });
+});
+
+gulp.task('build:deno', () => {
+  return gulp.src(['src/**/*.ts', '!src/util.ts']);
+})
